449f7a9b6de66a57a88446e05fe4c06d988fae68
Caroline Amanquah <carolineamanquah@Carolines-MacBook-Air.local>
2023-06-21 01:19:48 +0100

    creation of App.css file to style the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,84 @@
 // src/App.tsx
 
+// import React, { useState } from 'react';
+// import './App.css';
+
+
+// type Todo = {
+//   id: number;
+//   text: string;
+//   completed: boolean;
+// };
+
+// const App: React.FC = () => {
+//   const [todos, setTodos] = useState<Todo[]>([]);
+//   const [inputValue, setInputValue] = useState<string>('');
+
+//   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setInputValue(event.target.value);
+//   };
+
+//   const handleAddTodo = () => {
+//     if (inputValue.trim() === '') {
+//       return;
+//     }
+
+//     const newTodo: Todo = {
+//       id: Date.now(),
+//       text: inputValue,
+//       completed: false,
+//     };
+
+//     setTodos([...todos, newTodo]);
+//     setInputValue('');
+//   };
+
+//   const handleDeleteTodo = (id: number) => {
+//     setTodos(todos.filter((todo) => todo.id !== id));
+//   };
+
+//   const handleCompleteTodo = (id: number) => {
+//     setTodos(
+//       todos.map((todo) => {
+//         if (todo.id === id) {
+//           return { ...todo, completed: !todo.completed };
+//         } else {
+//           return todo;
+//         }
+//       })
+//     );
+//   };
+
+//   return (
+//     <div>
+//       <h1>To-Do List</h1>
+//       <input type="text" value={inputValue} onChange={handleInputChange} />
+//       <button onClick={handleAddTodo}>Add</button>
+//       <ul>
+//         {todos.map((todo) => (
+//           <li key={todo.id}>
+//             <input
+//               type="checkbox"
+//               checked={todo.completed}
+//               onChange={() => handleCompleteTodo(todo.id)}
+//             />
+//             <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+//               {todo.text}
+//             </span>
+//             <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// src/App.tsx
+
 import React, { useState } from 'react';
+import './App.css';
 
 type Todo = {
   id: number;
@@ -48,7 +126,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="app">
       <h1>To-Do List</h1>
       <input type="text" value={inputValue} onChange={handleInputChange} />
       <button onClick={handleAddTodo}>Add</button>
@@ -60,10 +138,10 @@ const App: React.FC = () => {
               checked={todo.completed}
               onChange={() => handleCompleteTodo(todo.id)}
             />
-            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            <span className={todo.completed ? 'completed' : ''}>
               {todo.text}
             </span>
-            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            <button className="delete-btn" onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
           </li>
         ))}
       </ul>
@@ -72,3 +150,5 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,43 @@
+// const path = require('path');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/index.tsx',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'index.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx|ts|tsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: [
+//               '@babel/preset-env',
+//               '@babel/preset-react',
+//               '@babel/preset-typescript',
+//             ],
+//           },
+//         },
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.tsx', '.ts', '.js', '.jsx'],
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'dist'),
+//     },
+//     compress: true,
+//     port: 3000,
+//   },
+// };
+
+
 const path = require('path');
 
 module.exports = {
@@ -22,6 +62,11 @@ module.exports = {
             ],
           },
         },
+      },
+      // Add a new rule for CSS files
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
